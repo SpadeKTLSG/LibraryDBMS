b76fe0c251df0b5a0dbbc8cc2dc6b28dea823671
SpadeKtlsg <spadekxcwxtlsg@gmail.com>
2023-12-08 10:40:12 +0800

    initial

---
+++ src/main/java/com/ldb/project/system/controller/SysRegisterController.java
@@ -0,0 +1,38 @@
+package com.ldb.project.system.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.ldb.common.utils.StringUtils;
+import com.ldb.framework.security.RegisterBody;
+import com.ldb.framework.security.service.SysRegisterService;
+import com.ldb.framework.web.controller.BaseController;
+import com.ldb.framework.web.domain.AjaxResult;
+import com.ldb.project.system.service.ISysConfigService;
+
+/**
+ * 注册验证
+ * 
+ * @author ruoyi
+ */
+@RestController
+public class SysRegisterController extends BaseController
+{
+    @Autowired
+    private SysRegisterService registerService;
+
+    @Autowired
+    private ISysConfigService configService;
+
+    @PostMapping("/register")
+    public AjaxResult register(@RequestBody RegisterBody user)
+    {
+        if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser"))))
+        {
+            return error("当前系统没有开启注册功能！");
+        }
+        String msg = registerService.register(user);
+        return StringUtils.isEmpty(msg) ? success() : error(msg);
+    }
+}
